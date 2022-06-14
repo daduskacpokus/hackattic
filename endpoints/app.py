cf957f1970f5c908a0a1b1bc1e4c5785bd7deedb
Vladyslav Tolstykh <daduskacpokus@gmail.com>
2022-06-14 18:13:13 +0300

    token

--- endpoints/app.py
+++ endpoints/app.py
@@ -1,10 +1,22 @@
-from flask import Flask, request
+from flask import Flask, request, abort
 app = Flask(__name__)
 
 @app.route('/challenges/backup_restore/problem', methods=['GET'])
 def say_get():
-  return 'Hello from Server'
+  token = request.args.get('access_token')
+  if (token == "x"):
+    return 'Hello from Server'
+  else:
+    abort(404)
   
 @app.route('/challenges/backup_restore/solve', methods=['POST'])
 def say_post():
-  return 'Hello from Server'
+  content_type = request.headers.get('Content-Type')
+  if (content_type == 'application/json'):
+    data = request.json
+    if (data['access_token'] == "x"):
+      return 'Hello from Server'
+    else:
+      abort(404)
+  else:
+    return 'Content-Type not supported!'  
